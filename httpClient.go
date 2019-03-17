dc697911e1e650e699b6522d25002ec745c1c2b6
Matthieu Jacquot <matthieujacquotdev@gmail.com>
2019-03-17 16:57:32 +0100

    add basic tests

---
+++ httpClient.go
@@ -0,0 +1,46 @@
+package client
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+type Quote struct {
+	Subject string `json:"subject"`
+	Author  string `json:"author"`
+	Text    string `json:"text"`
+}
+
+type HttpQuoteFetcher struct {
+	hostname string
+}
+
+func New(hostname string) *HttpQuoteFetcher {
+	return &HttpQuoteFetcher{hostname: hostname}
+}
+
+func (fetcher HttpQuoteFetcher) FetchQuote(subject string) (*Quote, error) {
+	response, err := http.Get(fetcher.hostname + "/api/" + subject)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode >= http.StatusBadRequest {
+		return nil, errors.New(response.Status)
+	}
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	quote := &Quote{}
+	if err := json.Unmarshal(respBody, &quote); err != nil {
+		return nil, err
+	}
+
+	return quote, nil
+}
---
+++ httpClient_test.go
@@ -0,0 +1,59 @@
+package client_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	client "github.com/err0r500/go-implementation-testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const subject = "art"
+
+var validQuote = &client.Quote{Author: "author", Text: "text", Subject: subject}
+
+func TestHttpQuoteFetcher_FetchQuote(t *testing.T) {
+	t.Run("happy case", func(t *testing.T) {
+		fakeServer := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/api/"+subject, r.URL.Path)
+				assert.Equal(t, http.MethodGet, r.Method)
+				validResponse(w, validQuote)
+			}),
+		)
+		defer fakeServer.Close()
+
+		quote, err := client.New(fakeServer.URL).FetchQuote(subject)
+		assert.NoError(t, err)
+		assert.Equal(t, validQuote, quote)
+	})
+
+	t.Run("response status error", func(t *testing.T) {
+		fakeServer := httptest.NewServer(
+			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.Error(w, "", http.StatusBadRequest)
+			}),
+		)
+		defer fakeServer.Close()
+
+		_, err := client.New(fakeServer.URL).FetchQuote(subject)
+		assert.Error(t, err)
+	})
+}
+
+func validResponse(w http.ResponseWriter, validQuote *client.Quote) {
+	w.WriteHeader(http.StatusOK)
+	resp, _ := json.Marshal(validQuote)
+	_, _ = w.Write(resp)
+}
+
+func getDummyServer() *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			validResponse(w, validQuote)
+		}),
+	)
+}
